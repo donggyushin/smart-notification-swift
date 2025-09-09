e125af567c75bbf865f0e5ad2f04a7c19f41cc61
Loopy <loopy@heyratel.com>
2025-09-09 15:03:18 +0900

    request permission

--- smart-notification-swift/Sources/SmartNotificationSwiftApp.swift
+++ smart-notification-swift/Sources/SmartNotificationSwiftApp.swift
@@ -1,12 +1,42 @@
 import SwiftUI
 import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
+    
+    // Request push notification permission
+    UNUserNotificationCenter.current().delegate = self
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, error in
+      if granted {
+        print("Push notification permission granted")
+      } else {
+        print("Push notification permission denied")
+      }
+    }
+    
+    application.registerForRemoteNotifications()
+    Messaging.messaging().delegate = self
 
     return true
+  }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                            willPresent notification: UNNotification,
+                            withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+      completionHandler([.sound, .list, .banner])
+  }
+}
+
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("FCM registration token: \(fcmToken ?? "")")
   }
 }
 
