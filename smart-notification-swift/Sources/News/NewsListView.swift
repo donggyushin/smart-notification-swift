d2a2fce83bba32b2db19a70e9b0c313733a7fa88
신동규 <donggyu9410@gmail.com>
2025-09-20 21:38:36 +0900

    Refact code

--- smart-notification-swift/Sources/News/NewsListView.swift
+++ smart-notification-swift/Sources/News/NewsListView.swift
@@ -13,44 +13,42 @@ struct NewsListView: View {
     let store: StoreOf<NewsListFeature>
     
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            List {
-                ForEach(store.news, id: \.id) { newsItem in
-                    NewsItemRow(newsItem: newsItem) { _ in
-                        viewStore.send(.saveNews(newsItem))
-                    }
-                    .onTapGesture {
-                        coordinator?.push(.news(newsItem.id))
-                    }
-                    .onAppear {
-                        if newsItem.id == store.news.last?.id {
-                            viewStore.send(.fetchNews)
-                        }
-                    }
+        List {
+            ForEach(store.news, id: \.id) { newsItem in
+                NewsItemRow(newsItem: newsItem) { _ in
+                    store.send(.saveNews(newsItem))
                 }
-                
-                if viewStore.loading {
-                    HStack {
-                        Spacer()
-                        ProgressView()
-                            .scaleEffect(1.2)
-                        Spacer()
+                .onTapGesture {
+                    coordinator?.push(.news(newsItem.id))
+                }
+                .onAppear {
+                    if newsItem.id == store.news.last?.id {
+                        store.send(.fetchNews)
                     }
-                    .padding()
                 }
             }
-            .scrollIndicators(.never)
-            .navigationTitle("Market News")
-            .refreshable {
-                viewStore.send(.reload)
-                viewStore.send(.saveCache)
-            }
-            .task {
-                if viewStore.news.isEmpty {
-                    viewStore.send(.prepareInitialData)
-                    viewStore.send(.fetchNews)
-                    viewStore.send(.saveCache)
+            
+            if store.loading {
+                HStack {
+                    Spacer()
+                    ProgressView()
+                        .scaleEffect(1.2)
+                    Spacer()
                 }
+                .padding()
+            }
+        }
+        .scrollIndicators(.never)
+        .navigationTitle("Market News")
+        .refreshable {
+            store.send(.reload)
+            store.send(.saveCache)
+        }
+        .task {
+            if store.news.isEmpty {
+                store.send(.prepareInitialData)
+                store.send(.fetchNews)
+                store.send(.saveCache)
             }
         }
     }
